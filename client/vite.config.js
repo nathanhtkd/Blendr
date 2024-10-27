2ad25f5270b03c127c58c5495b81f6f20b70f86c
Nathan Hartojo <nathanhartojo@gmail.com>
2024-10-27 10:55:49 -0500

    build to prod

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-});
+export default defineConfig(({ mode }) => ({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Separate dependencies into a 'vendor' chunk
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 800, // Adjust as needed
+  },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(mode === 'production' ? 'production' : 'development')
+  }
+}));
