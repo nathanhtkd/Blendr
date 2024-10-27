4214cb3fd117bc7e2a5ea98f2b494f45d10361f2
sarahlee1tamu <123109651+sarahlee1tamu@users.noreply.github.com>
2024-10-27 03:00:06 -0500

    Loading screen for extension page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      transitionProperty: {
+        'opacity': 'opacity',
+      },
+      transitionDuration: {
+        '1000': '1000ms',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
